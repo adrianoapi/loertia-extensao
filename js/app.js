c3ab3a3a997c2111237b5f659521c740288a21f4
Adriano API <adrianoapi@hotmail.com>
2019-07-28 17:35:24 -0300

    organizando script

--- js/app.js
+++ js/app.js
@@ -5,33 +5,33 @@
 **/
 var app = {
 	_jogo: 0,
-	_jogo_id: 0,
+	_sorteio: 0,
 	_data_sorteio: '',
 	_url: '',
 	get jogo() {
-		return this._jogo;
+            return this._jogo;
 	},
 	set jogo(value) {
-		this._jogo = value;
+            this._jogo = value;
 	},
-	get jogo_id() {
-		return this._jogo_id;
+	get sorteio() {
+            return this._sorteio;
 	},
-	set jogo_id(value) {
-		this._jogo_id = value;
+	set sorteio(value) {
+            this._sorteio = value;
 	},
 	get data_sorteio() {
-		return this._data_sorteio;
+            return this._data_sorteio;
 	},
 	set data_sorteio(value) {
-		var data = value.split('-');
-		this._data_sorteio = data[2]+"/"+data[1]+"/"+data[0];
+            var data = value.split('-');
+            this._data_sorteio = data[2]+"/"+data[1]+"/"+data[0];
 	},
 	get url() {
-		return this._url;
+            return this._url;
 	},
 	set url(value) {
-		this._url = value;
+            this._url = value;
 	},
         converteArray: function(string)
         {
--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 
-app.url = "http://www.maniadeganhar.com.br/api/ultimo-resultado";
-app.jogo = 2;
+app.url     = "http://www.maniadeganhar.com.br/api/ultimo-resultado";
+app.jogo    = 2;
 app.jogo_id = 2146;
 
 
 submitArticle(app.jogo);
 function submitArticle(value) {
-    console.log(value.valueOf());
+
     xhr = library.requisicaoHttp();
-    var data = "jogo=2&jogo_id=" + 2;
+    var data = "jogo=" + value + "&jogo_id=" + 2;
     xhr.open('POST', app.url, true);
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhr.send(data);
