253a280e249dbf95f37d2c1294c1ebe3272d3a7e
adrianoapi <adrianoapi@hotmail.com>
2019-08-08 16:35:52 -0300

    remove jqury and update layout button

--- js/resultado.js
+++ js/resultado.js
@@ -22,6 +22,15 @@ var resultado = {
                     this.contador = 3;
             }
         },
+        formatoMoeda: function(numero) {
+            numero = numero.replace(".", "");
+            var tmp = numero+'';
+            tmp = tmp.replace(/([0-9]{2})$/g, ",$1");
+            if( tmp.length > 6 ){
+                tmp = tmp.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
+            }
+            return tmp;
+        },
         templateHeader: function(value) {
             return '<p style="font-weight: bold;font-size: 13px; font-family: verdana;">' + value + '</p>';
         },
@@ -39,13 +48,12 @@ var resultado = {
             for(var i = 1; i <= this.contador; i++){
                 pontos     += this.template(i);
                 ganhadores += this.template(array['ganhador_'+i]);
-                premio     += this.template(array['rateio_'+i]);
+                premio     += this.template(this.formatoMoeda(array['rateio_'+i]));
             }
             
             document.getElementById('primeira').innerHTML = pontos;
             document.getElementById('segunda' ).innerHTML = ganhadores;
             document.getElementById('terceira').innerHTML = premio;
-            
             
         }
 };
