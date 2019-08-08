c770bf1daa3ea8bc8754f334eca29da024c91fc6
adrianoapi <adrianoapi@hotmail.com>
2019-08-08 17:17:09 -0300

    add template of score

--- js/resultado.js
+++ js/resultado.js
@@ -5,22 +5,34 @@
 **/
 var resultado = {
 	_jogo: 0,
-	_sorteio: 0,
         contador: 0,
         quantidade: function(jogo) {
             switch(jogo) {
                 case '1':
                     this.contador = 5;
+                    this._jogo    = 1;
                     break;
                 case '4':
                     this.contador = 4;
+                    this._jogo    = 4;
                     break;
                 case '3':
-                    this.contador = 6;
+                    this.contador = 7;
+                    this._jogo    = 3;
                     break;
                 default:
                     this.contador = 3;
+                    this._jogo    = 2;
             }
+        },
+        pontos: function(numero) {
+            var obj = {
+                1 : {1 : 15, 2 : 14, 3 : 13, 4 : 12, 5 : 11},
+                2 : {1 : 5,  2 : 4,  3 : 3},
+                3 : {1 : 20, 2 : 19, 3 : 18, 4 : 17, 5 : 16, 6 : 15, 7 : 0},
+                4 : {1 : 5,  2 : 4,  3 : 3,  4 : 2},
+            };
+            return obj[this._jogo][numero];
         },
         formatoMoeda: function(numero) {
             numero = numero.replace(".", "");
@@ -41,12 +53,12 @@ var resultado = {
             
             this.quantidade(array['jogo_id']);
             
-            var pontos     = this.templateHeader('PONTOS');
+            var pontos     = this.templateHeader('PONTOS'    );
             var ganhadores = this.templateHeader('GANHADORES');
-            var premio     = this.templateHeader('PRÊMIO');
+            var premio     = this.templateHeader('PRÊMIO'    );
             
             for(var i = 1; i <= this.contador; i++){
-                pontos     += this.template(i);
+                pontos     += this.template(this.pontos(i));
                 ganhadores += this.template(array['ganhador_'+i]);
                 premio     += this.template(this.formatoMoeda(array['rateio_'+i]));
             }
